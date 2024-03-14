f0de3e955069c4bad7e81ebbb24ba61a45e54044
4nderech <rech.anderson@hotmail.com>
2024-03-14 16:05:33 -0300

    1.0

---
+++ modelo/conexao.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+//const options = {
+//    useUnifiedTopology: true,
+//    useNewUrlParser: true
+//};
+
+mongoose.connect('mongodb://localhost:27017/');
+
+module.exports = mongoose;
---
+++ modelo/livro-dao.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const uri = 'mongodb://localhost:27017';
+
+mongoose.connect(uri);
+
+const livroSchema = new mongoose.Schema({
+  codigo: Number,
+  codEditora: Number,
+  titulo: String,
+  resumo: String,
+  autores: [String]
+});
+
+const Livro = mongoose.model('Livro', livroSchema);
+
+const obterLivros = async () => {
+  const livros = await Livro.find();
+  return { data: livros };
+};
+
+const incluir = async (livro) => {
+  const novoLivro = new Livro(livro);
+  const resultado = await novoLivro.save();
+  return resultado;
+};
+
+const excluir = async (codigo) => {
+  const resultado = await Livro.deleteOne({ _id: codigo });
+  return resultado;
+};
+
+module.exports = {
+  Livro,
+  obterLivros,
+  incluir,
+  excluir
+};
